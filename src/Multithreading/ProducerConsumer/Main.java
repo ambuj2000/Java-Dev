93a034c0973598812cc3ce5f58075fcb02f7c9ba
ambuj2000 <ambuj9748@gmail.com>
2024-08-10 00:12:48 +0530

    ProducerConsumer & InterCommunication

--- src/Multithreading/ProducerConsumer/Main.java
+++ src/Multithreading/ProducerConsumer/Main.java
@@ -10,3 +10,19 @@ public class Main {
         c.start();
     }
 }
+
+//Output:
+//Produced : 1
+//Consumed :1
+//Produced : 2
+//Consumed :2
+//Produced : 3
+//Consumed :3
+//Produced : 4
+//Consumed :4
+//Produced : 5
+//Consumed :5
+//Produced : 6
+//Consumed :6
+//Produced : 7
+//...continues untill broken
